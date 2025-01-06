4dd3eaa87ff01845b4d81cd443b2cfe6bf015b4a
Michael Ikehi <ikehi@Michaels-MacBook-Air.local>
2025-01-06 02:44:57 +0100

    Initial commit

--- src/app/dashboard/components/UserOverview.tsx
+++ src/app/dashboard/components/UserOverview.tsx
@@ -15,12 +15,7 @@ const UserOverview: React.FC = () => {
       try {
         const response = await fetch('https://fintech-dashbaord.vercel.app/api/users');
         const data = await response.json();
-        // Ensure recentTransactions is an array, fallback to empty array if not
-        const userData = {
-          ...data,
-          recentTransactions: data.recentTransactions || [],
-        };
-        setUser(userData);
+        setUser(data[0]);
       } catch (error) {
         console.error('Error fetching user data:', error);
       }
@@ -43,23 +38,23 @@ const UserOverview: React.FC = () => {
 
       {/* Recent Transactions */}
       <div>
-        <h4 className="text-xl font-semibold mb-4 text-black">Recent Transactions</h4>
-        <ul className="space-y-4">
-          {user.recentTransactions && Array.isArray(user.recentTransactions) && user.recentTransactions.length > 0 ? (
-            user.recentTransactions.map((transaction) => (
-              <li
-                key={transaction.id}
-                className={`flex justify-between text-sm p-3 rounded-lg
-                  ${transaction.type === 'credit' ? 'bg-green-50 text-green-700' : 'bg-red-50 text-red-700'}`}
-              >
-                <span>{transaction.date}</span>
-                <span>{transaction.type === 'credit' ? '+' : '-'}${transaction.amount && !isNaN(transaction.amount) ? transaction.amount.toFixed(2) : '0.00'}</span>
-              </li>
-            ))
-          ) : (
-            <p>No transactions available.</p>
-          )}
-        </ul>
+          <h4 className="text-xl font-semibold mb-4 text-black">Recent Transactions</h4>
+          <ul className="space-y-4">
+            {user.recentTransactions && Array.isArray(user.recentTransactions) && user.recentTransactions.length > 0 ? (
+              user.recentTransactions.map((transaction) => (
+                <li
+                  key={transaction.id}
+                  className={`flex justify-between text-sm p-3 rounded-lg
+                    ${transaction.type === 'credit' ? 'bg-green-50 text-green-700' : 'bg-red-50 text-red-700'}`}
+                >
+                  <span>{transaction.date}</span>
+                  <span>{transaction.type === 'credit' ? '+' : '-'}${transaction.amount && !isNaN(transaction.amount) ? transaction.amount.toFixed(2) : '0.00'}</span>
+                </li>
+              ))
+            ) : (
+              <p>No transactions available.</p>
+            )}
+          </ul>
       </div>
     </div>
   );
